0bbb2c85aaed828fbd66b89e691af59b31a12f34
Osvaldo Josué <osvaldojosuecruz@gmail.com>
2024-01-24 23:26:35 -0300

    Correcion en logo

--- src/componentes/Navbar/Navbar.jsx
+++ src/componentes/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
             <nav className="navbar navbar-expand-lg navbar-light w-75">
                 <div className="container-fluid">
                     <Link className='text-decoration-none' to={"/"}>
-                        <img src="\public\img\logo_blanco.webp" alt="Logo" width={200} />
+                        <img src="\img\logo_blanco.webp" alt="Logo" width={200} />
                     </Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
